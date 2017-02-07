b99f52f9249215b5c597885cdd726c48ddd191e3
Phil <phil.pichet@gmail.com>
2017-02-07 11:42:21 +0100

    Création des vues index et chapitre

---
+++ src/Controller/Controller.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace App\Controller;
+
+
+class Controller
+{
+    private $loader;
+    private $twig;
+
+    public function __construct()
+    {
+        $this->loader = new \Twig_Loader_Filesystem(__DIR__.'/../Resources/views');
+        $this->twig = new \Twig_Environment($this->loader, array('cache' => __DIR__.'/../../var/cache', 'debug' =>true));
+    }
+
+    /**
+     * @param $view
+     * @param array $params
+     * Fonction génerique permettant d'afficher une vue twig
+     */
+    protected function render($view, $params = [])
+    {
+        $this->template = $this->twig->load($view);
+        echo $this->template->render($params);
+    }
+}
